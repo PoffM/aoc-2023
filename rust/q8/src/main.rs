ac51a9a2a65f0de8e1259fe43dda9ad3342a07f7
PoffM <matp@live.ca>
2024-03-08 18:32:10 -0500

    q8 p2

--- rust/q8/src/main.rs
+++ rust/q8/src/main.rs
@@ -1,9 +1,5 @@
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-};
-
 use regex::Regex;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Dir {
@@ -36,11 +32,32 @@ fn main() {
         }
     }
 
-    // get first 3 chars
+    println!(
+        "Solution 1: {}",
+        steps_from(&nodes, &dirs, "AAA", Box::new(|node| node == "ZZZ"))
+    );
+
+    let start_nodes: Vec<&String> = nodes.keys().filter(|k| k.ends_with("A")).collect();
+
+    let step_counts = start_nodes.iter().map(|cursor| {
+        steps_from(&nodes, &dirs, cursor, Box::new(|node| node.ends_with("Z"))) as i128
+    });
+
+    let step_lcm = lcm(&step_counts.collect::<Vec<i128>>());
+
+    println!("Solution 2: {}", step_lcm);
+}
+
+fn steps_from(
+    nodes: &HashMap<String, (String, String)>,
+    dirs: &Vec<Dir>,
+    from: &str,
+    to: Box<dyn Fn(&str) -> bool>,
+) -> i32 {
     let mut steps = 0;
-    let mut node = "AAA";
+    let mut node = from;
     'outer: loop {
-        for dir in &dirs {
+        for dir in dirs {
             let (left, right) = nodes.get(node).unwrap();
             // println!("Node {} {:?}, Dir: {:?}", node, nodes.get(node), dir);
             match dir {
@@ -49,11 +66,27 @@ fn main() {
             }
             steps += 1;
 
-            if node == "ZZZ" {
+            if to(node) {
                 break 'outer;
             }
         }
     }
 
-    println!("Steps: {}", steps);
+    steps
+}
+
+fn lcm(nums: &[i128]) -> i128 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: i128, b: i128) -> i128 {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
